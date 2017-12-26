377f82c455d1ebc17c06f15c4a79e7128c4b6082
Mohammed Khalil Ait Brahim <mohammed.khalil.ab@gmail.com>
2017-12-26 16:19:27 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# A new way to select problems
+
+This project aims at helping you find the problems you will benefit the most from and track your progress because we believe it is rewarding.
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "/assets/",
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ],
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    devServer: {
+        historyApiFallback: true,
+        noInfo: true,
+        overlay: true
+    },
+    performance: {
+        hints: false
+    },
+    devtool: '#eval-source-map'
+}
