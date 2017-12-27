a8d80c4ceb6e4ffbb2fc749d17d86e29f51197f0
Mohammed Khalil Ait Brahim <mohammed.khalil.ab@gmail.com>
2017-12-27 16:52:04 +0000

    Better looking ProblemList and Bulma setup completed.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+/*
+ * styling related imports
+ * mainly referencing bulma and bulma related extensions.
+ */
+import './styles/main.scss'
 
 new Vue({
   el: '#app',
--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,22 @@ module.exports = {
                 ],
             },
             {
+              test: /\.scss$/,
+              use: [
+                'vue-style-loader',
+                'css-loader',
+                'sass-loader'
+              ],
+            },
+            {
+                test: /\.sass$/,
+                use: [
+                  'vue-style-loader',
+                  'css-loader',
+                  'sass-loader?indentedSyntax'
+                ],
+            },
+            {
                 test: /\.vue$/,
                 loader: 'vue-loader'
             },
